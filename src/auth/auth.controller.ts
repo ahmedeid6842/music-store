c0c05d8e5d2131bbe323ee26f7bb04c34cade946
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-12-05 05:42:12 +0200

    Add register endpoint to AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
-    
+    constructor(private authService: AuthService) { }
+
+    @Post('register')
+    async register(@Body() body: CreateUserDto) { 
+        return await this.authService.register(body);
+    }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly userService: UsersService) { }
+
+    async register(user: CreateUserDto) {
+    }
+}
