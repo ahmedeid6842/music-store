c14e0711ab3781e657a29e75f8080d1a804103e9
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-12-05 07:30:33 +0200

    Add NotLoggedGuard to register, verify-email, login routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,23 @@
-import { Body, Controller, Param, Post, Session } from '@nestjs/common';
+import { Body, Controller, Param, Post, Session, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { VerifyEmailDto } from './dto/verify-email.dto';
 import { LoginUserDto } from './dto/login-user.dto';
 import { ResetPasswordDto } from './dto/reset-password.dto';
+import { NotLoggedGuard } from './guards/not-logged.guard';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
 
     @Post('register')
+    @UseGuards(NotLoggedGuard)
     async register(@Body() body: CreateUserDto, @Session() session: any) {
         return await this.authService.register(body);
     }
 
     @Post('verify-email')
+    @UseGuards(NotLoggedGuard)
     async verifyEmail(@Body() { email, verificationCode }: VerifyEmailDto, @Session() session: any) {
         const user = await this.authService.verifyEmail(email, verificationCode);
         
@@ -23,7 +26,8 @@ export class AuthController {
         return user;
     }
   
-    @Post('/login')
+    @Post('login')
+    @UseGuards(NotLoggedGuard)
     async login(@Body() body: LoginUserDto, @Session() session: any) {
         const user = await this.authService.login(body);
 
@@ -32,7 +36,7 @@ export class AuthController {
         return user
     }
 
-    @Post('/reset-password')
+    @Post('reset-password')
     async askResetPassword(@Body() body: any) {
         await this.authService.sendResetPasswordEmail(body)
     }
