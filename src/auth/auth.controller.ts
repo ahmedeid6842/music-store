bb3ea0ed70a8f318cf9cd6c84ef90330da586c39
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-12-05 06:53:01 +0200

    Add session support to register and verifyEmail endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Session } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { VerifyEmailDto } from './dto/verify-email.dto';
@@ -8,12 +8,16 @@ export class AuthController {
     constructor(private authService: AuthService) { }
 
     @Post('register')
-    async register(@Body() body: CreateUserDto) {
+    async register(@Body() body: CreateUserDto, @Session() session: any) {
         return await this.authService.register(body);
     }
 
     @Post('verify-email')
-    async verifyEmail(@Body() { email, verificationCode }: VerifyEmailDto) {
-        return await this.authService.verifyEmail(email, verificationCode);
+    async verifyEmail(@Body() { email, verificationCode }: VerifyEmailDto, @Session() session: any) {
+        const user = await this.authService.verifyEmail(email, verificationCode);
+        
+        session.userId = user.id;
+
+        return user;
     }
 }
