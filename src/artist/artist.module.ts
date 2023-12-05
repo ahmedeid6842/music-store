cbba45272738859619cba37cd98c2c12710a65aa
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-12-06 01:08:43 +0200

    Add CurrentArtistMiddleware to ArtistModule

--- src/artist/artist.module.ts
+++ src/artist/artist.module.ts
@@ -1,13 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ArtistController } from './artist.controller';
 import { ArtistService } from './artist.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Artist } from './artist.entity';
 import { AuthModule } from 'src/auth/auth.module';
+import { CurrentArtistMiddleware } from './middleware/current-artist.middleware';
 
 @Module({
   imports:[TypeOrmModule.forFeature([Artist]),AuthModule],
   controllers: [ArtistController],
-  providers: [ArtistService]
+  providers: [ArtistService],
+  exports: [ArtistService]
 })
-export class ArtistModule {}
+export class ArtistModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(CurrentArtistMiddleware)
+      .forRoutes("*");
+  }
+}
