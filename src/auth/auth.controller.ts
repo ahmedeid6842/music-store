412470af5ef1d4131599fa37ecaf0d3a7b258e90
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-12-05 06:56:05 +0200

    Add login functionality to AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post, Session } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { VerifyEmailDto } from './dto/verify-email.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -17,7 +18,16 @@ export class AuthController {
         const user = await this.authService.verifyEmail(email, verificationCode);
         
         session.userId = user.id;
-
+        
         return user;
+    }
+  
+    @Post('/login')
+    async login(@Body() body: LoginUserDto, @Session() session: any) {
+        const user = await this.authService.login(body);
+
+        session.userId = user.id;
+
+        return user
     }
 }
