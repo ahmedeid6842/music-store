4d8a1bbc47c18df4a9070daf286d4f87d99b98a3
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-12-05 09:14:14 +0200

    Add createArtist endpoint to ArtistController

--- src/artist/artist.controller.ts
+++ src/artist/artist.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { UserDto } from 'src/auth/dto/user.dto';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { ArtistService } from './artist.service';
+import { CreateArtistDto } from './dto/create-artist.dto';
 
+@Serialize(UserDto)
 @Controller('artist')
-export class ArtistController {}
+export class ArtistController {
+    constructor(private artistService: ArtistService) { }
+
+    @UseGuards(AuthGuard)
+    @Post("/")
+    async createArtist(@Body() body: CreateArtistDto, @CurrentUser() user: UserDto) {
+        return this.artistService.createArtist(body, user);
+    }
+}
--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Artist } from './artist.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { UserDto } from 'src/auth/dto/user.dto';
 
 @Injectable()
-export class ArtistService {}
+export class ArtistService {
+    constructor(@InjectRepository(Artist) private artistRepo: Repository<Artist>) { }
+
+    async createArtist(body: CreateArtistDto, user: UserDto) {
+        //create artist 
+    }
+}
