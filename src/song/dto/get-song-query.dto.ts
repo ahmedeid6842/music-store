fa933bff143069e0242457aba98a7ae7d3b3bd9f
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-12-06 04:57:36 +0200

    Add GetSongQueryDto for song module

---
+++ src/song/dto/get-song-query.dto.ts
@@ -0,0 +1,40 @@
+import { IsIn, IsInt, IsNumber, IsOptional, IsString, IsUUID, Length, Min } from "class-validator";
+import { Transform } from "class-transformer";
+
+export class GetAlbumQueryDto {
+    @IsUUID('4')
+    @IsOptional()
+    id?: string;
+
+    @IsUUID('4')
+    @IsOptional()
+    albumId?: string;
+
+    @IsString()
+    @IsOptional()
+    @Length(1, 255)
+    title?: string;
+
+    @IsNumber()
+    @IsInt()
+    @IsOptional()
+    @Min(1)
+    @Transform(({ value }) => parseInt(value))
+    page?: number = 1;
+
+    @IsNumber()
+    @IsInt()
+    @IsOptional()
+    @Min(1)
+    @Transform(({ value }) => parseInt(value))
+    limit?: number = 10;
+
+    @IsString()
+    @IsOptional()
+    @IsIn(['ASC', 'DESC'])
+    sortOrder?: 'ASC' | 'DESC';
+
+    @IsString()
+    @IsOptional()
+    sortField?: string;
+}
